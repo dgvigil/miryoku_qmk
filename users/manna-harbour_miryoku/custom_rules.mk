9d7d01149eba8ce0caf676e19f3f1034645aab29
Dave Vigil <dgvigil@gmail.com>
2023-08-25 23:40:16 -0500

    Squeezing the most out of AVR

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -2,4 +2,9 @@
 # https://github.com/manna-harbour/miryoku
 
 CONSOLE_ENABLE = no
+LTO_ENABLE = yes
 AUDIO_ENABLE = no
+EXTRAFLAGS+=-flto
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
